b2d5bd0f08cc7579bf5bc981289a7a46d2ed9c79
ywarezk <ywarezk@gmail.com>
2021-09-30 20:58:43 +0300

    answered the question about di

--- 11-dependency-injection/di-tutorial/src/stam/stam.module.ts
+++ 11-dependency-injection/di-tutorial/src/stam/stam.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    // HttpClientModule
   ],
   providers: [
     {
       provide: 'hello',
       useValue: 'hello from StamModule'
+    },
+    /*
+    {
+      provide: HTTP_INTERCEPTORS,
+      ...
     }
+    */
   ]
 })
 export class StamModule { }
